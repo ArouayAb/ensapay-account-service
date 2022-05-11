96fe68c855e716085525b0eff6ef5923fba2a6c7
ArouayAb <arouayab@gmail.com>
2022-05-11 15:43:10 +0100

    Added register agent into the permit all filter

--- src/main/java/ensa/ebanking/accountservice/Security/SecurityConfig.java
+++ src/main/java/ensa/ebanking/accountservice/Security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/api/login/**", "/api/auth/refresh/**", "/api/account/client/register/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/login/**", "/api/auth/refresh/**", "/api/account/client/register/**", "/api/account/agent/register/**").permitAll();
         http.authorizeRequests().antMatchers("/api/account/client/currentUser", "/api/account/client/change-password").hasAnyAuthority("ROLE_CLIENT", "ROLE_AGENT");
         http.authorizeRequests().antMatchers("/api/account/client/**").hasRole("AGENT");
         http.authorizeRequests().antMatchers("/api/**").authenticated();
