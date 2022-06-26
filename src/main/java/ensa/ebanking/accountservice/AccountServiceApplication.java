9c6104c475e470a08ed958e4196d083b689b7435
Bader Toumi <55552093+bader-t@users.noreply.github.com>
2022-06-27 00:58:29 +0100

    clear inputs in AccountServiceApplication

--- src/main/java/ensa/ebanking/accountservice/AccountServiceApplication.java
+++ src/main/java/ensa/ebanking/accountservice/AccountServiceApplication.java
@@ -1,32 +1,14 @@
 package ensa.ebanking.accountservice;
-
-import ensa.ebanking.accountservice.DAO.ClientProfileDAO;
-import ensa.ebanking.accountservice.DAO.CreanceDAO;
-import ensa.ebanking.accountservice.DAO.CreancierDAO;
-import ensa.ebanking.accountservice.DAO.ServiceProviderDAO;
 import ensa.ebanking.accountservice.DTO.AdminProfileDTO;
-import ensa.ebanking.accountservice.DTO.AgentProfileDTO;
-import ensa.ebanking.accountservice.DTO.ClientProfileDTO;
-import ensa.ebanking.accountservice.Entities.Creance;
-import ensa.ebanking.accountservice.Enums.CreanceStatus;
-import ensa.ebanking.accountservice.Enums.ProductType;
-import ensa.ebanking.accountservice.Helpers.BankAccountHelper;
 import ensa.ebanking.accountservice.Services.AdminService;
-import ensa.ebanking.accountservice.Services.AgentService;
 import ensa.ebanking.accountservice.Services.CMIService;
-import ensa.ebanking.accountservice.Services.ClientService;
-import ensa.ebanking.accountservice.Helpers.EmailHelper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.sql.Date;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 @EnableScheduling
